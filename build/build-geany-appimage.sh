f1596f2239142d03ae0deef7ca3e3a980ca884bb
kamilnovo <kamilnovo@seznam.cz>
2026-02-21 21:28:42 +0100

    Fix: install meson and download Geany sources

--- build/build-geany-appimage.sh
+++ build/build-geany-appimage.sh
@@ -5,14 +5,29 @@ APPDIR="$PWD/AppDir"
 mkdir -p "$APPDIR"
 
 ############################################
-# 1) Build Geany
+# Install build dependencies
+############################################
+
+sudo apt-get update
+sudo apt-get install -y meson ninja-build pkg-config build-essential \
+    libgtk-3-dev libglib2.0-dev libvte-2.91-dev
+
+############################################
+# Download Geany source if not present
+############################################
+
+if [ ! -d geany-2.0 ]; then
+    echo "Downloading Geany 2.0..."
+    wget https://download.geany.org/geany-2.0.tar.bz2
+    tar xf geany-2.0.tar.bz2
+fi
+
+############################################
+# Build Geany
 ############################################
 
 echo "=== Building Geany ==="
-GEANY_DIR=$(find . -maxdepth 1 -type d -name "geany-*" | head -n 1)
-echo "Detected Geany directory: $GEANY_DIR"
-cd "$GEANY_DIR"
-
+cd geany-2.0
 
 meson setup build --prefix=/usr
 meson compile -C build
